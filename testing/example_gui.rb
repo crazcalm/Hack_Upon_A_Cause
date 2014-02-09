60c407f4ff5ff4cc4ad2952d3b282791346e017c
Marcus Willock <crazcalm@gmail.com>
2014-02-09 09:03:53 -0500

    How do I make two guis interact

--- testing/example_gui.rb
+++ testing/example_gui.rb
@@ -3,32 +3,42 @@
 require 'gtk2'
 
 window = Gtk::Window.new
-window.set_size_request(200, 100)
+window.set_size_request(300, 150)
 window.title = "GTK Entry"
 window.signal_connect("destroy") {Gtk.main_quit}
 
 vbox = Gtk::VBox.new(false, 0)
 window.add(vbox)
 
+hbox = Gtk::HBox.new(false, 0)
+vbox.add(hbox)
+
+hbox1 = Gtk::HBox.new(false, 0)
+vbox.add(hbox1)
+
+
+
+email_label = Gtk::Label.new("Email: ")
+hbox.pack_start(email_label, true, true, 0)
+
 entry = Gtk::Entry.new
 entry.max_length = 50
 entry.signal_connect("activate") {puts "Entry contents: #{entry.text}"}
 entry.text = "Enter gmail email here"
 entry.select_region(0, -1)
-vbox.pack_start(entry, true, true, 0)
+hbox.pack_start(entry, true, true, 0)
 
-hbox = Gtk::HBox.new(false, 0)
-vbox.add(hbox)
+pass_label = Gtk::Label.new("Password:")
+hbox1.pack_start(pass_label, true, true, 0)
 
-check = Gtk::CheckButton.new("Editable")
-check.signal_connect("toggled") {|w| entry.editable = w.active?}
-check.active = entry.editable?
-hbox.pack_start(check, true, true, 0)
+pass = Gtk::Entry.new
+pass.max_length = 50
+pass.signal_connect("activate") {puts "Entry contents: #{pass.text}"}
+pass.text = "Give me your password!"
+pass.visibility = false
+#entry.select_region(0, -2)
+hbox1.pack_start(pass,true,true, 0)
 
-check = Gtk::CheckButton.new("Visible")
-check.signal_connect("toggled") {|w| entry.visibility = w.active?}
-check.active = entry.visibility?
-hbox.pack_start(check, true, true, 0)
 
 button = Gtk::Button.new("Submit")
 button.signal_connect("clicked") do
